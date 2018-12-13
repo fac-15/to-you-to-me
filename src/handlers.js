988f38af473036b253729510ca4c97073ed6e0e9
jcharnley <jasoncharnley@gmail.com>
2018-12-13 11:08:01 +0000

    created postnew user fucntion and altered argument names in handlers

---
+++ src/handlers.js
@@ -0,0 +1,98 @@
+const { readFile } = require("fs");
+const path = require("path");
+const qs = require("qs");
+const jwt = require("jsonwebtoken");
+const http = require("http");
+
+const getData = require("./queries/getdata.js");
+const postData = require("./queries/postdata.js");
+
+const serverError = (err, res) => {
+  res.writeHead(500, ("Content-Types": "text/html"));
+  res.end("<h1>Sorry there was a problem loading..</h1>");
+};
+
+const homeHandler = res => {
+  const filePath = path.join(__dirname, "..", "public", "index.js");
+  readFile(filePath, (err, file) => {
+    if (err) return serverError(err, res);
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(file);
+  });
+};
+
+const publicHandler = (url, res) => {
+  const filepath = path.join(__dirname, "..", url);
+  readFile(filepath, (err, file) => {
+    if (err) return serverError(err, res);
+    const [extension] = url.split(".")[1];
+    const extensionType = {
+      html: "text/html",
+      css: "text/css",
+      js: "application/javascript",
+      ico: "image/x-icon"
+    };
+    response.writeHead(200, { "Content-Type": extensionType[extension] });
+    response.end(file);
+  });
+};
+
+const loginHandler = (requestMethod, requestUrl) => {
+  return readFile("./html/login.html", (err, file) => {
+    res.writeHead(302, {
+      "Content-Type": "text/html",
+      "Content-Length": data.length,
+      Location: "/",
+      "Set-Cookie": `jwt=${token}; HttpOnly; Max-Age=1000`
+    });
+    return res.end(file);
+  });
+};
+
+const logoutHandler = (requestMethod, requestUrl) => {
+  return readFile("./html/logout.html", (err, file) => {
+    res.writeHead(302, {
+      "Content-Type": "text/html",
+      "Content-Length": data.length,
+      Location: "/",
+      "Set-Cookie": `jwt=0; HttpOnly; Max-Age=0`
+    });
+    return res.end(file);
+  });
+};
+
+const registerHandler = (req, res) => {
+  // console.log("hey, we're about to register!");
+  let data = "";
+  request.on("data", chunk => {
+    data += chunk;
+  });
+  request.on("end", () => {
+    const { name, userName, email, pass } = qs.parse(data);
+
+    postNewUser(name, userName, email, pass, err => {
+      if (err) return serverError(err, response);
+      response.writeHead(302, { Location: "/" });
+      response.end();
+    });
+  });
+};
+
+const hobbyHandler = (req, res) => {
+  // console.log("we're getting HOBBIES!");
+  getData((err, res) => {
+    if (err) return serverError(err, response);
+    let data = JSON.stringify(res);
+    response.writeHead(200, { "Content-Type": "application/json" });
+    response.end(data);
+  });
+};
+
+module.exports = {
+  homeHandler,
+  publicHandler,
+  loginHandler,
+  logoutHandler,
+  registerHandler,
+  hobbyHandler
+};
---
+++ src/queries/postdata.js
@@ -0,0 +1,19 @@
+const dbConnection = require("../database/db_connection.js");
+
+const postNewUser = (name, userName, email, password, cb) => {
+  dbConnection.query(
+    "INSERT into users (full_name, username, email, password) VALUES ($1, $2, $3, $4)",
+    [name, userName, email, password],
+    (err, res) => {
+      if (err) {
+        return cb(err);
+      } else {
+        cb(null, res);
+      }
+    }
+  );
+};
+
+module.exports = {
+  postNewUser
+};
