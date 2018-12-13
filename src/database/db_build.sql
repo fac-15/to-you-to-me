d1d1f6f6ea8ef00897d3851be696ffbadeb03c27
Lucy <lewright20@gmail.com>
2018-12-13 10:23:32 +0000

    created and populated getData file; populated user table with two examply users for example hobbies

--- src/database/db_build.sql
+++ src/database/db_build.sql
@@ -16,22 +16,25 @@ CREATE TABLE IF NOT EXISTS hobbies (
     location VARCHAR(255) NOT NULL,
     type VARCHAR(255) NOT NULL,
     user_id INTEGER REFERENCES users(id) NOT NULL,
-    comments VARCHAR(255) NOT NULL    
+    comments VARCHAR(255)    
 );
 
-CREATE TABLE IF NOT EXISTS reviews (
-    id SERIAL PRIMARY KEY,
-    rating INTEGER,
-    likes INTEGER,
-    reviews VARCHAR(255),
-    user_id INTEGER REFERENCES users(id) NOT NULL,
-    hobbies_id INTEGER REFERENCES hobbies(id) NOT NULL
-);
+-- CREATE TABLE IF NOT EXISTS reviews (
+--     id SERIAL PRIMARY KEY,
+--     rating INTEGER,
+--     likes INTEGER,
+--     reviews VARCHAR(255),
+--     user_id INTEGER REFERENCES users(id) NOT NULL,
+--     hobbies_id INTEGER REFERENCES hobbies(id) NOT NULL
+-- );
 
-INSERT INTO hobbies (hobbie_name, location, type) VALUES ('Bouldering', 'Biscuit Factory', 'physical');
-INSERT INTO hobbies (hobbie_name, location, type) VALUES ('Cinema', 'Peckhamplex', 'leisure');
-INSERT INTO hobbies (hobbie_name, location, type) VALUES ('Pottery', 'Biscuit Factory', 'creative');
-INSERT INTO hobbies (hobbie_name, location, type) VALUES ('Open mic night', 'Comedy Club', 'musical');
-INSERT INTO hobbies (hobbie_name, location, type) VALUES ('The Lion King', 'Lyceum Theatre', 'theatre');
+INSERT INTO users (full_name, username, email, password) VALUES ('Lucy Wright', 'lucylu', 'lewright20@gmail.com', 'abcde');
+INSERT INTO users (full_name, username, email, password) VALUES ('Kristina Jaggard', 'teenie', 'jaggardk@gmail.com', 'abcde');
+
+INSERT INTO hobbies (hobbie_name, location, type, user_id) VALUES ('Bouldering', 'Biscuit Factory', 'physical', 1);
+INSERT INTO hobbies (hobbie_name, location, type, user_id) VALUES ('Cinema', 'Peckhamplex', 'leisure', 2);
+INSERT INTO hobbies (hobbie_name, location, type, user_id) VALUES ('Pottery', 'Biscuit Factory', 'creative', 1);
+INSERT INTO hobbies (hobbie_name, location, type, user_id) VALUES ('Open mic night', 'Comedy Club', 'musical', 1);
+INSERT INTO hobbies (hobbie_name, location, type, user_id) VALUES ('The Lion King', 'Lyceum Theatre', 'theatre', 2);
 
 COMMIT;
