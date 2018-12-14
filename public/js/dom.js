80747c082000a8e6bfc52eb72d8aaefdaf7382ad
Cynthia Onwordi <cynthiaonwordi@Cynthias-MacBook-Pro.local>
2018-12-14 12:14:29 +0000

    front style changes. added page for new route

--- public/js/dom.js
+++ public/js/dom.js
@@ -4,10 +4,28 @@ const getHobbies = (response) => {
   let hobbiesContainer = document.getElementById('hobbies');
   response.forEach((hobby) => {
     console.log('This is the getHobbies response: ', response);
-    const hobbyEntry = document.createElement('div');
+
+    //DOM Elements
+    const hobbyEntry = document.createElement('li');
+    hobbyEntry.setAttribute("class", "hobbie_item");
+
+    // const hobbyNames = document.createElement('p');
+    // hobbyNames.setAttribute("class", "hobbie_name");
+    //
+    const hobbyLoc = document.createElement('p');
+    hobbyLoc.setAttribute("class", "hobbie_loc");
+    // const hobbyEntry = document.createElement('div');
+    // hobbyEntry.setAttribute("id", "hobbie_item");
+
     const hobbyName = hobby.hobbie_name;
+    const hobbyLocation = hobby.location;
+    // const hobbyType = hobby.type;
+
     hobbyEntry.textContent = hobbyName;
     hobbiesContainer.appendChild(hobbyEntry);
+
+    hobbyLoc.textContent = hobbyLocation;
+    hobbyEntry.appendChild(hobbyLoc);
   });
 };
 
